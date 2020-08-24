4d8904ba0baacbee7cbaba40467cd44020c6a5a9
Marcio Klepacz <marcioklepacz@gmail.com>
2020-08-24 17:52:02 -0300

    Correct source name

--- lib/trianongastao.rb
+++ lib/trianongastao.rb
@@ -30,7 +30,7 @@ module Trianongastao
                 name: lead.product.name
               },
               source: {
-                name: lead.source.name
+                name: "#{source.name} - Gastão"
               }
             }
           },
